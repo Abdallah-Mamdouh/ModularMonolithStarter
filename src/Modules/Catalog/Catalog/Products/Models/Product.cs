0d2a5b318291bb30a57f0e7745137279c5b07875
hkhaled <hkhaled@DEV-001>
2026-03-30 11:09:53 +0200

    first commit

---
+++ src/Modules/Catalog/Catalog/Products/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using Shared.DDD;
+
+namespace Catalog.Products.Models
+{
+    public class Product : Entity<Guid>
+    {
+        public string Name { get; set; } = default!;
+        public List<string> Categories { get; set; } = new();
+        public string Description { get; set; } = default!;
+        public string ImageFile { get; set; } = default!;
+        public decimal Price { get; set; }
+
+        public static Product Create(Guid id ,string name,  List<string> categories, string description, string imageFile, decimal price)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(name);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+            Product product = new Product
+            {
+                Id = id,
+                Name = name,
+                Categories = categories,
+                Description = description,
+                ImageFile = imageFile,
+                Price = price
+            };
+        }
+    }
+}
