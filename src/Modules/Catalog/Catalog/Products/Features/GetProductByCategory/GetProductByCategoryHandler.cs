6cc6cccbe7254f02c35a655b9fae5e38bbabf7cf
hkhaled <hkhaled@DEV-001>
2026-04-04 16:57:59 +0200

    Add product query handlers and integrate Mapster mapping
    Introduced GetProductsHandler and GetProductByCategoryHandler to retrieve and map products using Mapster. Updated dependencies to include Mapster and cleaned up unused folder includes in Catalog.csproj.

---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,28 @@
+﻿
+using Catalog.Data;
+
+namespace Catalog.Products.Features.GetProductByCategory
+{
+    public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+
+    public record GetProductByCategoryResult(IEnumerable<ProductDto> Products);
+
+    internal class GetProductByCategoryHandler(CatalogDbContext catalogDbContext) : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            // get products from database
+            // return them as a result
+
+            var products = await catalogDbContext.Products
+                .AsNoTracking()
+                .Where(p => p.Categories.Contains(query.Category))
+                .OrderBy(p => p.Name)
+                .ToListAsync(cancellationToken);
+
+            var productDtos = products.Adapt<List<ProductDto>>();
+
+            return new GetProductByCategoryResult(productDtos);
+        }
+    }
+}
---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace Catalog.Products.Features.GetProducts
+{
+    public record GetProductsQuery : IQuery<GetProductsResult>;
+
+    public record GetProductsResult(IEnumerable<ProductDto> Products);
+
+    internal class GetProductsHandler(CatalogDbContext catalogDbContext) : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            // get products from database
+            // return them as a result
+
+            var products = await catalogDbContext.Products
+                .AsNoTracking()
+                .OrderBy(p => p.Name)
+                .ToListAsync(cancellationToken);
+
+            var productDtos = products.Adapt<List<ProductDto>>();
+
+            return new GetProductsResult(productDtos);
+        }
+    }
+}
