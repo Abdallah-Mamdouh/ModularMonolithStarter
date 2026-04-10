2502ffb71cb38a389eb7820254de16ac3aae9916
hkhaled <hkhaled@DEV-001>
2026-04-10 11:14:40 +0200

    feat(basket): initialize basket module structure and add shopping cart DTOs
    Add folder structure for basket features (Create, Delete, Get, Checkout, etc.)
    Introduce ShoppingCartDto and ShoppingCartItemDto
    Prepare module for future event handlers and exceptions

--- src/Modules/Basket/Basket/BasketModule.cs
+++ src/Modules/Basket/Basket/BasketModule.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Data;
+using Shared.Data.Interceptors;
 
 namespace Basket
 {
@@ -9,11 +12,38 @@ namespace Basket
         public static IServiceCollection AddBasketModule (this IServiceCollection services,
             IConfiguration configuration)
         {
+            // Add services to the container
+
+            // Api endpoint services
+
+            // Application use case services
+
+            // Data - infrastructure services
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+            services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+            services.AddDbContext<BasketDbContext>((sp, options) =>
+            {
+                options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+                options.UseNpgsql(connectionString);
+            });
+
             return services;
         }
 
         public static IApplicationBuilder UseBasketModule (this IApplicationBuilder app)
         {
+            // Configure the HTTP request pipeline.
+
+            // Use Api endpoint services
+
+            // Use Application use case services
+
+            // Use Data - infrastructure services
+            app.UseMigration<BasketDbContext>();
+
             return app;
         }
     }
---
+++ src/Modules/Basket/Basket/Data/Configuration/ShoppingCartConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Basket.Data.Configuration
+{
+    public class ShoppingCartConfiguration : IEntityTypeConfiguration<ShoppingCart>
+    {
+        public void Configure(EntityTypeBuilder<ShoppingCart> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.UserName)
+                    .IsUnicode();
+
+            builder.Property(p => p.UserName)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+            builder.HasMany(p => p.Items)
+                .WithOne()
+                .HasForeignKey(p => p.ShoppingCartId);
+        }
+    }
+}
