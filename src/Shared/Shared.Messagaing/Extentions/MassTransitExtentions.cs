6a7353e052c58e2935053a0e2753628a13bce7f0
Abdallah-Mamdouh <119abdallah118@gmail.com>
2026-04-19 09:43:59 +0200

    feat: introduce shared messaging layer
    - Created Shared.Messaging project
    - Added project reference to Basket and Catalog
    - Implemented IntegrationEvent base class for integration events

---
+++ src/Shared/Shared.Messagaing/Extentions/MassTransitExtentions.cs
@@ -0,0 +1,29 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Shared.Messagaing.Extentions
+{
+    public static class MassTransitExtentions
+    {
+        public static IServiceCollection AddMassTransitWithAssemblies
+        (this IServiceCollection services, IConfiguration configuration, params Assembly[] assemblies)
+        {
+            // Implement MassTransit Configuration
+            services.AddMassTransit(config =>
+            {
+                config.SetKebabCaseEndpointNameFormatter();
+
+                config.SetInMemorySagaRepositoryProvider();
+
+                config.AddConsumers(assemblies);
+                config.AddSagaStateMachines(assemblies);
+                config.AddSagas(assemblies);
+                config.AddActivities(assemblies);
+            });
+
+            return services;
+        }
+    }
+}
